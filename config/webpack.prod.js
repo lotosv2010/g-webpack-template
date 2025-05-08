3229b753c66c43d3663b10734344c3f04e08b4d3
Robin <lotosv2010@163.com>
2025-05-08 10:35:17 +0800

    feat(cource-map): 生产环境source-map最佳实践

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -8,7 +8,7 @@ const WebpackBar = require('webpackbar');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 const WebpackLogPlugin = require('./webpack-log-plugin');
 
 module.exports = merge(common,{
@@ -52,6 +52,27 @@ module.exports = merge(common,{
       analyzerMode: 'static',      // 生成静态HTML报告
       reportFilename: path.resolve(__dirname, '..', 'reports', 'index.html'), // 输出路径（相对于 output.path）
       openAnalyzer: Boolean(process.env.OPEN_ANALYZER)         // 是否自动打开浏览器
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map[query]',
+      // TODO 正式上线时，append 需要修改为本地地址
+      append: '\n//# sourceMappingURL=http://127.0.0.1:7070/[url]',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            // TODO 正式上线时，destination 需要修改为正式上线的目录
+            { source: './dist/**/*.map', destination: './sourcemap', options: { overwrite: true, flat: true }}
+          ],
+          delete: [
+            './dist/**/*.map',
+          ],
+          archive: [
+            { source: './dist', destination: './dist/dist.zip' },
+          ],
+        },
+      },
     }),
     new WebpackLogPlugin()
   ]
